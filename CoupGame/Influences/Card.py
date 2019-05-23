42686fee3f7f85085f77a0a125b249059ef4ac0a
Thomas van Dongen <thomas123@live.nl>
2019-05-23 15:03:08 +0200

    Added class cards

---
+++ CoupGame/Influences/Card.py
@@ -0,0 +1,4 @@
+class Card:
+	def __init__(self, Type):
+		self.Type = Type
+		#self.actions = []
---
+++ CoupGame/Influences/Duke.py
@@ -0,0 +1,8 @@
+import Card
+
+class Duke(Card):
+	def __init__(self):
+		self.Type = "Duke"
+		self.Actions = ["Tax", "BlockFA"]
+
+

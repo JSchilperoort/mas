546298e3bc861798da76a28199dd3d00c3bedf4b
ninojansen <ninojansen5@gmail.com>
2019-06-19 14:00:18 +0200

    Remove index

--- CoupGame/Interface2.py
+++ CoupGame/Interface2.py
@@ -6,6 +6,7 @@ class MainApplication(tk.Frame):
     def __init__(self, root, *args, **kwargs):
         tk.Frame.__init__(self, root, *args, **kwargs)
         self.root = root
+        self.img_ref = []
         self.initUI()
 
     def initUI(self):
@@ -20,8 +21,8 @@ class MainApplication(tk.Frame):
         game_frame.grid(row=2, column=1, sticky="ew")
         
         tk.Label(title_frame, text="Coup Game v1.0", bg='#F4EBE8', font=("Helvetica ", 22)).grid(sticky="EW")
-
-        n_players = 3
+        
+        n_players = 4
         player_frames = []
         for i in range(n_players):
             if i % 2 == 0:
@@ -29,18 +30,33 @@ class MainApplication(tk.Frame):
             else:
                 color = "green"
 
+            if i == 0:
+                sticky = "nw"
+            elif i == n_players:
+                sticky= "ew"
+            else:
+                sticky = "n"
+
             player_frame = tk.Frame(game_frame, width=window_w/n_players, bg=color, bd=4, height=400)
-            player_frame.grid(row=1, column=i+1, sticky="NEW")
+            player_frame.grid(row=1, column=i+1, sticky=sticky)
             player_frames.append(player_frame)
 
-        #for player_frame in player_frames:
-        player_frame = player_frames[0] 
-        card_image = Image.open("images/duke.jpg")
-        card_image = card_image.resize((175, 250), Image.ANTIALIAS)
-        card_image = ImageTk.PhotoImage(card_image)
+        for i, player_frame in enumerate(player_frames):
+            tk.Label(player_frame, text="Player {}".format(i+1)).grid(row=1, column=1)
+            tk.Label(player_frame, image=self.load_image("images/duke.jpg", size=(175, 250))).grid(row=2, column=1)
+            tk.Label(player_frame, image=self.load_image("images/duke.jpg", size=(175, 250))).grid(row=2, column=2)
+            tk.Label(player_frame, text="10").grid(row=3, column=1, sticky="w")
+            tk.Label(player_frame, image=self.load_image("images/coin.jpg", size=(25,25))).grid(row=3, column=2, sticky="w")
 
-        tk.Label(player_frame, image=card_image).grid(row=1, column=1)
-        tk.Label(player_frame, image=card_image).grid(row=1, column=2)
+    def load_image(self, path, size=None):
+        image = Image.open(path)
+        if size is not None:
+            image = image.resize(size, Image.ANTIALIAS)
+        image = ImageTk.PhotoImage(image)
+        self.img_ref.append(image)
+        return image
+        
+        
 
 
 
